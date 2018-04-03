b12fd228aa52a1a0e1dcb4973bbce2d6bed1ec17
Léo Martin <leo.martin.epitech@gmail.com>
2018-04-03 14:43:37 +0200

    updated appveyor

--- src/main/index.js
+++ src/main/index.js
@@ -96,7 +96,7 @@ app.on('activate', () => {
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
 
-/*
+
 import { autoUpdater } from 'electron-updater'
 
 autoUpdater.on('update-downloaded', () => {
@@ -106,4 +106,4 @@ autoUpdater.on('update-downloaded', () => {
 app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
- */
+
